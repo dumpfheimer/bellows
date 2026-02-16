b285e832aeb7a85d6788d2c91fb74725a3915206
Christopher Hoch <chris.hoch@hpc.at>
2026-02-16 09:44:11 +0100

    test

--- bellows/ezsp/v9/__init__.py
+++ bellows/ezsp/v9/__init__.py
@@ -41,4 +41,5 @@ class EZSPv9(EZSPv8):
 
     async def set_source_route(self, nwk: t.NWK, relays: list[t.NWK]) -> t.sl_Status:
         # While the command may succeed, it does absolutely nothing
-        return t.sl_Status.OK
+        (res,) = await self.setSourceRoute(destination=nwk, relayList=relays)
+        return t.sl_Status.from_ember_status(res)
